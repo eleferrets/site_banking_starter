22044dfaf72dc72e7251ba2a7fa9e0471aff5a46
elefe <eleferrets@gmail.com>
2021-06-16 13:58:31 -0400

    Everything is set up for use

--- bank-of-codepath-express-api/utils/errors.js
+++ bank-of-codepath-express-api/utils/errors.js
@@ -1,20 +1,20 @@
 class ExpressError extends Error {
   constructor(message, status) {
-    super()
-    this.message = message
-    this.status = status
+    super();
+    this.message = message;
+    this.status = status;
   }
 }
 
 class NotFoundError extends ExpressError {
   constructor(message = "Not Found") {
-    super(message, 404)
+    super(message, 404);
   }
 }
 
 class BadRequestError extends ExpressError {
   constructor(message = "Bad Request") {
-    super(message, 400)
+    super(message, 400);
   }
 }
 
@@ -22,4 +22,4 @@ module.exports = {
   ExpressError,
   NotFoundError,
   BadRequestError,
-}
+};
